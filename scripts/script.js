0e492d38f2ad26327f65b2bae1680186b548dfe4
Reubenadbs <reubenadbs@gmail.com>
2025-12-05 14:05:11 +0100

    pushhhh

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+var navButton = document.querySelector("nav div:first-of-type");
+
+navButton.onclick = toggleMenu;
+
+function toggleMenu(){
+    var nav = document.querySelector("nav div:nth-of-type(2)")
+    nav.classList.toggle("toonMenu")
+}
